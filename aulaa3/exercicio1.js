90318d485ad552fae280d4a7d2cbeb7e6f148e22
Berenice <berenicelopesmaria@gmal.com>
2021-11-10 23:49:07 -0300

    exercicios - parte 2

---
+++ aulaa3/exercicio1.js
@@ -0,0 +1,17 @@
+/*
+1. Soma entre 3 + 7
+2. Subtração entre 135 e 29
+3. Multiplicação entre 4 e 8
+4. Divisão entre 225 e 5
+*/
+var soma = 3+7;
+console.log(soma);
+
+var subtracao = 135 - 29;
+console.log(subtracao);
+
+var multiplicao = (4 * 8);
+console.log(multiplicao);
+
+var divisao = 225 / 5;
+console.log(divisao);
---
+++ aulaa3/exercicio2.js
@@ -0,0 +1,21 @@
+/*
+var saldo = 50;
+var saldoComBonus = saldo + 20;
+var saldoComDesconto = saldoComBonus / 0.5;
+var saldoFinal = saldoComDesconto;
+*/
+var saldo = 50;
+console.log(saldo);
+
+var saldoComBonus = saldo + 20;
+console.log(saldoComBonus);
+
+var saldoComDesconto = saldoComBonus / 0.5;
+console.log(saldoComDesconto);
+
+var saldoFinal = saldoComDesconto;
+console.log(saldoFinal);
+
+
+
+
