8cdeb69335a13f558a5f7325f74929430e75e9ce
Berenice <berenicelopesmaria@gmal.com>
2021-11-11 01:01:58 -0300

    exercicios - parte 3

---
+++ aulaaa3/exercicio1.js
@@ -0,0 +1,20 @@
+/*pseudocodigo
+if (*condição é verdadeira*) {
+*instrução é executada*
+}
+Exemplo
+var minhaVariavel = true;
+if (minhaVariavel) {
+  console.log("é verdade")
+} else {
+	console.log("é falso)
+}
+*/
+var hojeVaiChover = false;
+
+if (hojeVaiChover) {
+
+  console.log("Leve seu guarda-chuva, hoje vai chover");
+} else {
+    console.log("Fique tranquilo, hoje será um dia se sol")
+}
---
+++ aulaaa3/exercicio2.js
@@ -0,0 +1,22 @@
+
+var maior = 5 > 1;
+console.log(maior);
+
+var menor = 2000000<3000000000;
+console.log(menor);
+
+var igual = "Banana" == "Banana";
+console.log(igual);
+
+var estritamente = "50" === 50;
+console.log(estritamente);
+
+var diferente = "Carro" != "bicicleta";
+console.log(diferente);
+
+var calcularMaior = 50 > 27;
+console.log(calcularMaior);
+
+var calcularMenor = 975 <= 1200;
+console.log(calcularMenor);
+
