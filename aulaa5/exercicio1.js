431688e2db89254f4ec55a18876f8299cee2ceb2
Berenice <berenicelopesmaria@gmal.com>
2021-11-15 20:50:00 -0300

    exercícios - parte 1

---
+++ aulaa5/exercicio1.js
@@ -0,0 +1,11 @@
+
+const mvaloresNu = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const criaPares = mvaloresNu.filter(function(numero)  {
+    return numero % 2 === 0
+});
+    console.log(criaPares);
+
+
+
+
+
